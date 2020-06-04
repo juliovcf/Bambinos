07ee34613a2f621ae416d6028601c3ac8b639e0d
juliovcf <52920962+juliovcf@users.noreply.github.com>
2020-06-04 13:08:57 +0200

    - Clase Java Plato - ListView en Platos

---
+++ app/src/main/java/com/example/bambino/Plato.java
@@ -0,0 +1,16 @@
+package com.example.bambino;
+
+public class Plato {
+    private String nombre;
+    private double precio;
+
+    public Plato(){
+
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+
+}
--- app/src/main/java/com/example/bambino/Platos.java
+++ app/src/main/java/com/example/bambino/Platos.java
@@ -1,14 +1,35 @@
 package com.example.bambino;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class Platos extends AppCompatActivity {
+    DatabaseReference databaseReference;
+    ListView listView;
+    ArrayList<String> arrayList = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_platos);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Pizzas");
+        listView = (ListView) findViewById(R.id.lPlatos);
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
